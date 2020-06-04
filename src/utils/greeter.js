0684fcd443fdc9504121e7a21c00913aacf8bda9
OziOcb <pawel_ozi@o2.pl>
2020-06-04 12:30:40 +0100

    Add tests for the Greeter class

--- src/utils/greeter.js
+++ src/utils/greeter.js
@@ -1,3 +1,48 @@
-class Greeter {}
+/**
+    1. Write a Greeter class with greet function that receives a name as input and outputs Hello <name>.
+        The signature of greet should not change throughout the kata.
+        You are allowed to construct Greeter object as you please.
+    2. greet trims the input
+    3. greet capitalizes the first letter of the name
+    4. greet returns Good morning <name> when the time is 06:00-12:00
+    5. greet returns Good evening <name> when the time is 18:00-22:00
+    6. greet returns Good night <name> when the time is 22:00-06:00
+    7. greet logs into console each time it is called
+ */
+
+class Greeter {
+  constructor(currentTimeInMillisecondsFn) {
+    this.currentTimeInMillisecondsFn = currentTimeInMillisecondsFn;
+  }
+
+  greet(name) {
+    const trimName = name.trim();
+    const theName = trimName.charAt(0).toUpperCase() + trimName.slice(1).toLowerCase();
+
+    const currentTime = new Date(this.currentTimeInMillisecondsFn());
+    const currentHour = currentTime.getHours();
+
+    const greeting = `${this._getGreeting(currentHour)} ${theName}`;
+    console.log(greeting);
+
+    return greeting;
+  }
+
+  _getGreeting(currentHour) {
+    let greeting;
+
+    if (currentHour >= 6 && currentHour < 12) {
+      greeting = "Good morning";
+    } else if (currentHour >= 18 && currentHour < 22) {
+      greeting = "Good evening";
+    } else if (currentHour >= 22 || currentHour < 6) {
+      greeting = "Good night";
+    } else {
+      greeting = "Hello";
+    }
+
+    return greeting;
+  }
+}
 
 module.exports = Greeter;
--- tests/unit/greeter.spec.js
+++ tests/unit/greeter.spec.js
@@ -1,7 +1,77 @@
 import Greeter from "@/utils/greeter";
 
-describe("sanity", () => {
-  test("sanity ", () => {
-    expect(typeof Greeter).toBe("function");
+describe("Greeter class", () => {
+  let subject;
+  let date;
+
+  beforeEach(() => {
+    date = new Date();
+    date.setHours(14);
+
+    subject = new Greeter(() => date.getTime());
+  });
+
+  test("1. receive a name as input and outputs Hello <name>", () => {
+    const inputs = ["Paul", "Aga", "Dawid"];
+
+    inputs.forEach((input) => {
+      expect(subject.greet(input)).toBe(`Hello ${input}`);
+    });
+  });
+
+  test("2. greet trims the input", () => {
+    const input = "   Paul  ";
+
+    expect(subject.greet(input)).toBe("Hello Paul");
+  });
+
+  test("3. greet capitalizes only the first letter of the name", () => {
+    const inputs = ["paul", "agA", "daWid"];
+    const outputs = ["Hello Paul", "Hello Aga", "Hello Dawid"];
+
+    inputs.forEach((input, index) => {
+      expect(subject.greet(input)).toBe(outputs[index]);
+    });
+  });
+
+  test("4. greet returns Good morning <name> when the time is 06:00-12:00", () => {
+    date.setHours(6);
+    expect(subject.greet("Paul")).toEqual("Good morning Paul");
+
+    date.setHours(11);
+    expect(subject.greet("Bob")).toEqual("Good morning Bob");
+
+    date.setHours(12);
+    expect(subject.greet("Aga")).not.toEqual("Good morning Aga");
+  });
+
+  test("5. greet returns Good evening <name> when the time is 18:00-22:00", () => {
+    date.setHours(18);
+    expect(subject.greet("Paul")).toEqual("Good evening Paul");
+
+    date.setHours(21);
+    expect(subject.greet("Bob")).toEqual("Good evening Bob");
+  });
+
+  test("6. greet returns Good night <name> when the time is 22:00-06:00", () => {
+    date.setHours(22);
+    expect(subject.greet("Paul")).toEqual("Good night Paul");
+
+    date.setHours(5);
+    expect(subject.greet("Paul")).toEqual("Good night Paul");
+  });
+
+  test("7. greet logs into console each time it is called", () => {
+    const spy = jest.spyOn(console, "log");
+
+    subject.greet("Paul");
+    subject.greet("Bob");
+
+    expect(spy).toHaveBeenCalled();
+    expect(spy).toHaveBeenCalledTimes(2);
+    expect(spy).toHaveBeenCalledWith("Hello Paul");
+    expect(spy).toHaveBeenCalledWith("Hello Bob");
+
+    spy.mockRestore();
   });
 });
